9031a69c8c01952eac636a95fb10a726a9057691
John Servo <jaservo@up.edu.ph>
2022-06-12 19:30:13 +0800

    added the board pixels

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const board = document.getElementById("board");
 
+for(let i = 0; i < 256; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    board.append(pixel);
+}
+
+
+
+
+
+/*
+LESSONS LEARNED:
+
+> For every element created by javascript there is only one instance of it.
+If we want to create another element of the same type but with a different
+purpose, we have to create a different variable for that.
+
+*/
